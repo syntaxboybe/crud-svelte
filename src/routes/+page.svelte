b4f06e4588e38790d770e998c34c40cce112d507
syntaxboybe <beboymegalbioeyana@gmail.com>
2025-02-03 17:01:31 +0800

    fking commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to Cuy</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<p> this is only a demo for crudas </p>
